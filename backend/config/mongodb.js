5ee3d5a71b5be5ba3bbb240688e42634b9c039f7
Amine Trigui <aminetrigui.github@gmail.com>
2025-01-01 18:13:00 +0100

    Backend environment & D.B connection

---
+++ backend/config/mongodb.js
@@ -0,0 +1,7 @@
+import mongoose from "mongoose"
+
+const connectDB = async() => { 
+mongoose.connection.on('connected', () => console.log('DataBase connected')) 
+  await mongoose.connect(`${process.env.MONGODB_URI}/meditime`)
+ }
+export default connectDB
